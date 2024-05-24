759c6f5c29fc46d346ccae0aade8f770a3f223a0
Jonas Souza <jonassouza@live.com>
2024-05-24 13:30:26 -0300

    Criação da classe de contexto EF

---
+++ Sigti.Data/Base/SigtiContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Sigti.Data.Base
+{
+    public class SigtiContext:IdentityDbContext
+    {
+        public SigtiContext(DbContextOptions<SigtiContext> options):base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+    }
+}
