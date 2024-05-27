b9795abc2cf5a4da6f38dcc628592b6eebac4e92
Jonas Souza <jonassouza@live.com>
2024-05-27 14:47:38 -0300

    Correções e ajustes de notificação no repositório genérico

--- Sigti.Core/Interfaces/IGenericRepository.cs
+++ Sigti.Core/Interfaces/IGenericRepository.cs
@@ -11,9 +11,9 @@ namespace Sigti.Core.Interfaces
 {
     public interface IGenericRepository<T> where T : Entity
     {
-        Task CreateAsync(T entity);
-        Task UpdateAsync(T entity);
-        Task DeleteAsync(Guid id);
+        bool Create(T entity);
+        bool Update(T entity);
+       bool Delete(Guid id);
         Task<IEnumerable<T>> GetAllByIdAsync(Guid id);
         Task<IEnumerable<T>> GetAllAsync();
         Task<IEnumerable<T>> GetAllByExpressionAsync(Expression<Func<T,bool>> expression);
--- Sigti.Data/Base/GenericRepository.cs
+++ Sigti.Data/Base/GenericRepository.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Flunt.Notifications;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using Sigti.Core.Entities;
 using Sigti.Core.Interfaces;
 using System;
@@ -7,10 +9,11 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Sigti.Data.Base
 {
-    public class GenericRepository<T> : IGenericRepository<T> where T : Entity
+    public class GenericRepository<T> :Notifiable<Notification>, IGenericRepository<T> where T : Entity
     {
         private readonly SigtiContext _context;
         public GenericRepository(SigtiContext context)
@@ -18,47 +21,122 @@ namespace Sigti.Data.Base
             _context = context;
         }
 
-        public async Task CreateAsync(T entity)
+        public  bool Create(T entity)
         {
-            await _context.AddAsync(entity);
+            try
+            {
+                 _context.Add(entity);
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                AddNotification(new Notification("Create", $"{ex.Message}\n{ex.InnerException}"));
+                return false;
+            }
+         
+        }
+        public bool Update(T entity)
+        {
+            try
+            {
+                
+                var entityExists = _context.Set<T>().AsNoTracking().Contains(entity);
+                if (!entityExists) AddNotification(new Notification("Update", "Registro não localizado na base de dados!"));
+                else _context.Entry(entity).State = EntityState.Modified;
+                return true;
+                
+            }
+            catch (Exception ex)
+            {
+
+
+                AddNotification(new Notification("Update", $"{ex.Message}\n{ex.InnerException}"));
+                return false;
+            }
+
+         
         }
 
-    
 
-        public async Task DeleteAsync(Guid id)
+        public bool Delete(Guid id)
         {
-            var entity = await _context.FindAsync(typeof(Guid), id);
-            if (entity != null)
+            try
             {
-                //This will mark the Entity State as Deleted
-                _context.Remove(id);
+                var entity =  _context.Set<T>().Find(typeof(Guid), id);
+                if (entity == null) AddNotification(new Notification("Delete", "Registro não localizado na base de dados!"));
+                else _context.Set<T>().Remove(entity);
+                return true;
             }
+            catch (Exception e)
+            {
+
+                AddNotification(new Notification("Delete", $"{e.Message}\n" + e.InnerException));
+                return false;
+            }
+
         }
 
         public async Task<IEnumerable<T>> GetAllAsync()
         {
-            return await _context.Set<T>().AsNoTracking().ToListAsync();
+            try
+            {
+                return await _context.Set<T>().AsNoTracking().ToListAsync();
+            }
+            catch (Exception e)
+            {
+
+                AddNotification(new Notification("GetAll", $"{e.Message}\n" + e.InnerException));
+                return null;
+            }
+
         }
 
         public async Task<IEnumerable<T>> GetAllByExpressionAsync(Expression<Func<T, bool>> expression)
         {
-            return await _context.Set<T>().AsNoTracking().Where(expression).ToListAsync();
+            try
+            {
+                return await _context.Set<T>().AsNoTracking().Where(expression).ToListAsync();
+            }
+            catch (Exception e)
+            {
+
+                AddNotification(new Notification("GetAllByExpression", $"{e.Message}\n" + e.InnerException));
+                return null;
+            }
+
         }
 
         public async Task<IEnumerable<T>> GetAllByIdAsync(Guid id)
         {
-          return await _context.Set<T>().AsNoTracking().Where(t=>t.Id==id).ToListAsync();
+            try
+            {
+                return await _context.Set<T>().AsNoTracking().Where(t => t.Id == id).ToListAsync();
+            }
+            catch (Exception e)
+            {
+
+                AddNotification(new Notification("GetAllById", $"{e.Message}\n" + e.InnerException));
+                return null;
+            }
+         
         }
 
         public async Task<T> GetByIdAsync(Guid id)
         {
-            return await _context.Set<T>().AsNoTracking().FirstOrDefaultAsync(t => t.Id == id);
+            try
+            {
+                return await _context.Set<T>().AsNoTracking().FirstOrDefaultAsync(t => t.Id == id);
+            }
+            catch (Exception e)
+            {
+
+                AddNotification(new Notification(" GetById", $"{e.Message}\n" + e.InnerException));
+                return null;
+            }
+          
         }
 
-        public async Task UpdateAsync(T entity)
-        {
-
-            _context.Update(entity);
-        }
+      
     }
 }
