5296cc3fdae42e2de5df8e40e088cc12405c60e7
Jonas Souza <jonassouza@live.com>
2024-05-30 18:07:50 -0300

    Remoção de projeto web

---
+++ Sigti.Web/IComputadorService.cs
@@ -0,0 +1,24 @@
+﻿
+using Sigti.Application.DTO;
+using System.Net.Http.Json;
+
+namespace Sigti.Web
+{
+    public interface IComputadorService
+    {
+        Task<List<ListaComputadorGridDTO>?> GetAllAsync();
+
+    }
+    public class ComputadorService(IHttpClientFactory httpClientFactory) : IComputadorService
+    {
+        private readonly HttpClient _client = httpClientFactory.CreateClient("api");
+
+        public async Task<List<ListaComputadorGridDTO>?> GetAllAsync()
+
+     => await _client.GetFromJsonAsync<List<ListaComputadorGridDTO>?>("v2/Computadores")
+           ?? new List<ListaComputadorGridDTO>();
+    }
+
+
+}
+
