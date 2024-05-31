94d7915c414645face7fd0a8ea2bae1d93a0bdc1
Jonas Souza <jonassouza@live.com>
2024-05-30 21:53:44 -0300

    Remoção de projeto Wasm

---
+++ Sigti.Web/Services/IComputadorService.cs
@@ -0,0 +1,9 @@
+﻿using Sigti.Application.DTO;
+
+namespace Sigti.Web.Services
+{
+    public interface IComputadorService
+    {
+        Task<List<ListaComputadorGridDTO>> GetComputadores();
+    }
+}
